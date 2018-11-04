fcf3988a2ee0a88f1209dbf8b2e6b1b437bb265b
Feng Sao <wildsaofeng@gmail.com>
2018-11-04 22:32:05 +0800

    finished chapter 5

---
+++ Albums/index.ios.js
@@ -0,0 +1,15 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+
+const App = () => {
+  return (
+    <Header/>
+  ); 
+}
+
+// Render it to the device
+AppRegistry.registerComponent('Albums', () => App);
